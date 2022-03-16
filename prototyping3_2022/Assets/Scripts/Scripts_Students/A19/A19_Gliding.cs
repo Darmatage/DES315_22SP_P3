fc701e165aad7122b8637f226d5fae47fadde0d8
KobeDennis <kobe.dennis@digipen.edu>
2022-03-16 15:42:31 -0700

    Added Gliding to the A19 bot.
    If the player holds the jump button, gliding will be enabled.

---
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A19/A19_Gliding.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A19_Gliding : MonoBehaviour
+{
+    public BotBasic_Move botBasic_Move;
+    public Rigidbody rb;
+    public bool isGliding = false;
+    private float gravityWorld = -9.81f;
+    public float glideSpeed = 1f;
+    public float gravityScale = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Gliding();
+    }
+    private void  FixedUpdate()
+    {
+        if(botBasic_Move.isGrounded == false)
+        {
+            Vector3 gravity = gravityWorld * gravityScale * Vector3.up;
+            rb.AddForce(gravity, ForceMode.Acceleration);
+        }
+    }
+
+    public void Gliding()
+    {
+        if(!botBasic_Move.isGrounded)
+        {
+            if(Input.GetButton(botBasic_Move.pJump) && rb.velocity.y <= 0f)
+            {
+                isGliding = true;
+                rb.velocity = new Vector3(rb.velocity.x, -glideSpeed, rb.velocity.z);
+            }
+            else
+            {
+                isGliding = false;
+            }
+        }
+    }
+}
