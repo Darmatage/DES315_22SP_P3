bf5c1f38eb3e4b8954a6694d434becac774b4cad
Taro Kumagai <71030806+tckumaga@users.noreply.github.com>
2022-03-18 06:45:42 -0700

    Started Working on Bot, Implemented first draft of basic projectile

---
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A11_TaroKumagai/TaroKumagai_WeaponDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaroKumagai_WeaponDamage : HazardDamage
+{
+    public TaroKumagai_Weapon_BasicProjectile parentRef;
+	void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject != parentRef.gameObject)
+        {
+            SpawnParticlesHere = other.contacts[0].point;
+            //make particles
+            GameObject damageParticles = Instantiate(particlesPrefab, SpawnParticlesHere, other.transform.rotation);
+            StartCoroutine(destroyParticles(damageParticles));
+            Destroy(gameObject);
+            parentRef.activeProjectiles--;
+        }
+    }
+
+
+    IEnumerator destroyParticles(GameObject particles)
+    {
+		yield return new WaitForSeconds(0.5f);
+		Destroy(particles);
+	}
+
+}
+
+	//NOTE: this script is just damage
+	//hazard object movement is managed by their button
+	//reporting damage is done by the damage script on the bots 
---
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A11_TaroKumagai/TaroKumagai_Weapon_BasicProjectile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaroKumagai_Weapon_BasicProjectile : MonoBehaviour
+{
+
+	public GameObject projectile;
+
+    public int MaxProjectiles = 2;
+    public float ProjectileLifetime = 3f;
+    public float ProjectileLaunchForce = 10f;
+    public int activeProjectiles = 0;
+	//grab axis from parent object
+	public string button;
+
+
+    void Start(){
+		button = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
+        
+    }
+
+    void Update()
+    {
+		//if (Input.GetKeyDown(KeyCode.T)){
+		if (Input.GetButtonDown(button) && activeProjectiles < MaxProjectiles)
+        {
+            Vector3 LaunchPosition = gameObject.transform.position + gameObject.transform.forward * 2;
+            GameObject newProjectile = Instantiate(projectile, LaunchPosition, Quaternion.identity);
+            activeProjectiles++;
+            newProjectile.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * ProjectileLaunchForce, ForceMode.Impulse);
+            newProjectile.GetComponent<TaroKumagai_WeaponDamage>().parentRef = this;
+
+            if (gameObject.transform.root.tag == "Player1")
+                newProjectile.GetComponent<HazardDamage>().isPlayer1Weapon = true;
+            else
+                newProjectile.GetComponent<HazardDamage>().isPlayer2Weapon = true;
+
+            StartCoroutine(SetProjectileToExpire(newProjectile, ProjectileLifetime));
+        }
+    }
+
+	IEnumerator SetProjectileToExpire(GameObject projectile, float projectileLifetime)
+    {
+		yield return new WaitForSeconds(projectileLifetime);
+        Destroy(projectile);
+    }
+
+}
