f1aacc3700c25dd7e33cd64fbf926038124813bf
MattWaka <Matt.Walker@digipen.edu>
2022-03-21 15:37:24 -0700

    MattWalker - growth added, jaw is falling off

--- prototyping3_2022/Assets/Scripts/Scripts_Students/B17_MattWalker/B17_JawMovement.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B17_MattWalker/B17_JawMovement.cs
@@ -26,6 +26,7 @@ public class B17_JawMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (IsReadyToBite)
 		{
             if (CheckMouthFullyClosed())
---
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B17_MattWalker/B17_LeechStats.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B17_LeechStats : MonoBehaviour
+{
+    private string thisPlayer;
+    public float ScaleIncreaseAmount = 1.1f;
+    public float ScaleIncreaseSpeed = 100f;
+    public bool IsLeeching;
+    //public float cooldown = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisPlayer = gameObject.transform.root.tag;
+        IsLeeching = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Transform otherRoot = other.transform.root;
+        
+        if (otherRoot.tag != thisPlayer)
+            if (otherRoot.tag == "Player1" || otherRoot.tag == "Player2")
+			{
+                IsLeeching = true;
+                StartCoroutine(IncreaseSize(transform.root.localScale * ScaleIncreaseAmount));
+			}
+    }
+
+    IEnumerator IncreaseSize(Vector3 targetScale)
+	{
+        while (transform.root.localScale.magnitude < targetScale.magnitude)
+		{
+            transform.root.localScale += transform.root.localScale * ScaleIncreaseSpeed * Time.deltaTime;
+            yield return null;
+        }
+
+        IsLeeching = false;
+	}
+}
