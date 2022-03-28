fc8e8701ae11de8307b25d712c58471b272c0c8f
Alex-Jalonen <71279927+Alex-Jalonen@users.noreply.github.com>
2022-03-28 10:53:13 -0700

    added lockdown ability animation

--- prototyping3_2022/Assets/Scripts/Scripts_Students/B18_AlexJalonen/B18_Movement.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B18_AlexJalonen/B18_Movement.cs
@@ -11,6 +11,7 @@ public class B18_Movement : MonoBehaviour
 	public float boostSpeed = 10f;
 
 	private Rigidbody rb;
+	private Animator anim;
 	public Transform groundCheck;
 	public Transform turtleCheck;
 	public LayerMask groundLayer;
@@ -25,12 +26,14 @@ public class B18_Movement : MonoBehaviour
 	// public float flipTime = 1f;
 
 	public bool isGrabbed = false;
+	public bool isAnchored = false;
 
 	//grab axis from parent object
 	public string parentName;
 	public string pVertical;
 	public string pHorizontal;
 	public string pJump;
+	public string button2;
 	public string button3;
 	public string button4; 
 
@@ -42,10 +45,16 @@ public class B18_Movement : MonoBehaviour
 			rb = gameObject.GetComponent<Rigidbody>();
 		}
 
+		if (gameObject.GetComponent<Animator>() != null)
+		{
+			anim = gameObject.GetComponent<Animator>();
+		}
+
 		parentName = this.transform.parent.gameObject.name;
 		pVertical = gameObject.transform.parent.GetComponent<playerParent>().moveAxis;
 		pHorizontal = gameObject.transform.parent.GetComponent<playerParent>().rotateAxis;
 		pJump = gameObject.transform.parent.GetComponent<playerParent>().jumpInput;
+		button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
 		button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
 		button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
 	}
@@ -53,20 +62,31 @@ public class B18_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		// JUMP
+		isGrounded = Physics.CheckSphere(groundCheck.position, 0.8f, groundLayer);
+		isTurtled = Physics.CheckSphere(turtleCheck.position, 0.4f, groundLayer);
+
+		if(isGrounded && !isAnchored && Input.GetButtonDown(button2))
+        {
+			Anchor();
+        }
+        else if(isAnchored && Input.GetButtonDown(button2))
+        {
+			UnAnchor();
+        }
+
 		float botStrafe = (-Input.GetAxisRaw(button3) + Input.GetAxisRaw(button4)) * moveSpeed * Time.deltaTime;
 		float botMove = Input.GetAxisRaw(pVertical) * moveSpeed * Time.deltaTime;
 		float botRotate = Input.GetAxisRaw(pHorizontal) * rotateSpeed * Time.deltaTime;
 
-		if (isGrabbed == false)
+		if (isGrabbed == false && isAnchored == false)
 		{
 			transform.Translate(botStrafe, 0, 0);
 			transform.Translate(0, 0, botMove);
 			transform.Rotate(0, botRotate, 0);
 		}
 
-		// JUMP
-		isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundLayer);
-		isTurtled = Physics.CheckSphere(turtleCheck.position, 0.4f, groundLayer);
+
 		if (Input.GetButtonDown(pJump))
 		{
 			if (isGrounded == true)
@@ -98,4 +118,16 @@ public class B18_Movement : MonoBehaviour
 			}
 		}
 	}
+
+	private void Anchor()
+    {
+		anim.SetTrigger("MoveFeetDown");
+		isAnchored = true;
+    }
+
+	private void UnAnchor()
+    {
+		anim.SetTrigger("MoveFeetUp");
+		isAnchored = false;
+    }
 }
