cfa4154a4e7d9a94eaf5dc6cfd131967b04d85dd
JulianBDigi <90929962+JulianBDigi@users.noreply.github.com>
2022-03-27 19:17:14 -0700

    Julian Blackstone - Made bot have combat capabilities and work with proper controls

--- prototyping3_2022/Assets/Scripts/Scripts_Students/B04_JulianBlackstone/JulianBlackstone_TankModeScript.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B04_JulianBlackstone/JulianBlackstone_TankModeScript.cs
@@ -11,20 +11,51 @@ public class JulianBlackstone_TankModeScript : MonoBehaviour
     [SerializeField]
     private GameObject leg3 = null;
     [SerializeField]
-    private GameObject leg4 = null;
+    private GameObject leg4 = null;    
+    [SerializeField]
+    private GameObject mace = null;
 
-    // Start is called before the first frame update
+
+    //grab axis from parent object
+    public string button1;
+    public string button2;
+    public string button3;
+    public string button4; // currently boost in player move script
+
     void Start()
     {
-        
+        button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
+        button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
+        button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
+        button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetButtonUp(button1))
+        {
+            Vector3 newPos = transform.position;
+            newPos.y += 2;
+            transform.position = newPos;
+        }
+
+        if (Input.GetButtonDown(button1))
+        {
+            Vector3 newPos = transform.position;
+            newPos.y -= 2;
+            transform.position = newPos;
+        }
+
+        if (Input.GetButton(button1))
         {
             BotBasic_Move myMove = GetComponent<BotBasic_Move>();
+
+            leg1.SetActive(false);
+            leg2.SetActive(false);
+            leg3.SetActive(false);
+            leg4.SetActive(false);
+            mace.SetActive(true);
 
             myMove.moveSpeed = 2;
             myMove.rotateSpeed = 250;
@@ -33,6 +64,14 @@ public class JulianBlackstone_TankModeScript : MonoBehaviour
         else
         {
             BotBasic_Move myMove = GetComponent<BotBasic_Move>();
+
+
+            leg1.SetActive(true);
+            leg2.SetActive(true);
+            leg3.SetActive(true);
+            leg4.SetActive(true);
+            mace.SetActive(false);
+
 
             myMove.moveSpeed = 15;
             myMove.rotateSpeed = 120;
