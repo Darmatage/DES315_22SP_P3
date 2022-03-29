0493ae1ab784e1c165c3ff4108ba3d7460a441c7
Jessa Grammar <j.gramer@digipen.edu>
2022-03-29 01:53:18 -0700

    JessicaGramer - Added animations

--- prototyping3_2022/Assets/Scripts/Scripts_Students/B03_JessicaGramer/B03_AnimationController.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B03_JessicaGramer/B03_AnimationController.cs
@@ -2,32 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class B03_AnimationController : MonoBehaviour
 {
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        animator = GetComponent<Animator>();
     }
 
     public void SheildDefend()
     {
+        if (!animator) return;
 
+        animator.SetBool(B03_AnimationSettings.Sheild, false);
     }
 
     public void WeaponEvent(B03Bot_Weapon.Weapon weapon)
     {
+        if (!animator) return;
 
+        if(weapon == B03Bot_Weapon.Weapon.SHEILD)
+        {
+            animator.SetBool(B03_AnimationSettings.Sheild, true);
+        }
+        else if(weapon == B03Bot_Weapon.Weapon.BOW)
+        {
+            animator.SetTrigger(B03_AnimationSettings.DrawBow);
+        }
+        else if(weapon == B03Bot_Weapon.Weapon.KNIVES)
+        {
+            animator.SetBool(B03_AnimationSettings.Daggers, true);
+        }
+        else
+        {
+            animator.SetBool(B03_AnimationSettings.Daggers, false);
+            animator.SetBool(B03_AnimationSettings.Sheild, false);
+        }
     }
 
     public void MovementEvent(float distance)
     {
+        if (!animator) return;
 
+        if (distance != 0) animator.SetBool(B03_AnimationSettings.IsWalking, true);
+        else animator.SetBool(B03_AnimationSettings.IsWalking, false);
     }
 }
