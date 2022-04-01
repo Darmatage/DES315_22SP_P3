4aa551fa5613ac967e5e1088663e1a5763af03be
Christian Wookey <christian.wookey@gmail.com>
2022-04-01 09:51:38 -0700

    Added more feedback

---
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A15_ChristianWookey/Boxer_HazardDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxer_HazardDamage : HazardDamage{
+	private GameHandler gameHandler;
+
+	public Material[] materials;
+
+	private float delay = 0f;
+
+    public void Update()
+    {
+		delay = Mathf.Max((delay - Time.deltaTime), 0f);
+	}
+
+    void OnCollisionEnter(Collision other) {
+		if (delay <= 0f)
+		{
+			GameObject damageParticles = Instantiate(particlesPrefab, transform.position + SpawnParticlesHere, other.transform.rotation);
+			damageParticles.GetComponent<ParticleSystemRenderer>().material = materials[0];// Random.Range(0, materials.Length)];
+			damageParticles.GetComponent<ParticleSystem>().Emit(1);
+			delay = 0.1f;
+			Destroy(damageParticles, 1f);
+		}
+	}
+
+}
+
+	//NOTE: this script is just damage
+	//hazard object movement is managed by their button
+	//reporting damage is done by the damage script on the bots 
