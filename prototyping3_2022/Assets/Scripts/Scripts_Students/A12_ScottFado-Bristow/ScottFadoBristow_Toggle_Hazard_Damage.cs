8f3d81f3e1e5e48ae59e51cbe5dc4eeda3a7349d
Scott Fado-Bristow <tfbscott@gmail.com>
2022-04-08 08:53:33 -0700

    -rebalance

--- prototyping3_2022/Assets/Scripts/Scripts_Students/A12_ScottFado-Bristow/ScottFadoBristow_Toggle_Hazard_Damage.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A12_ScottFado-Bristow/ScottFadoBristow_Toggle_Hazard_Damage.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class ScottFadoBristow_Toggle_Hazard_Damage : HazardDamage
 {
     private float startingDamage = 0f;
-
+    public bool DamageOn = true;
 
     new void Start()
     {
         base.Start();
 
         startingDamage = damage;
+
+        if (!DamageOn)
+            ToggleDamageOff();
     }
-    void ToggleDamage()
+    public void ToggleDamageOff()
     {
         
         damage = 0;
     }
 
 
-   
+    public void ToggleDamageOn()
+    {
+
+        damage = startingDamage;
+    }
+
+
 }
