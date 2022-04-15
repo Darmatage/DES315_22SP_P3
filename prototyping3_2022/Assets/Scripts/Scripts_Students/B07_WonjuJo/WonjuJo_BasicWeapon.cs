2b4bde8f74b4745fad5a833abec608facb77e2c1
WonJu Jo <wonju.cho@digipen.edu>
2022-04-15 03:17:32 -0700

    Change button1 skill and add new sfx for the skill
    delete first weapon

--- prototyping3_2022/Assets/Scripts/Scripts_Students/B07_WonjuJo/WonjuJo_BasicWeapon.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B07_WonjuJo/WonjuJo_BasicWeapon.cs
@@ -5,37 +5,36 @@ public class WonjuJo_BasicWeapon : MonoBehaviour
 {
 	//NOTE: This script goes on the main playerBot Game Object, and the weapon goes in the public GO slot
 
-	public GameObject weaponThrust;
+	public GameObject FrontBarrel;
+	public GameObject BackBarrel;
+	public GameObject BackProjectileLauncher;
+	public GameObject FrontProjectileLauncher;
 	public GameObject Projectile;
-	public GameObject ProjectileLauncher;
-	public GameObject Barrel;
-
-	private float thrustAmount = 3.5f;
-
-	private bool weaponOut = false;
 
 	//grab axis from parent object
 	public string button1;
 	public string button2;
 	public string button3;
 	public string button4; // currently boost in player move script
-
-	private float Button1Cooldown = 0f;
-	private float Button1CooldownRate = 2f;
 	
 	private float Button2Cooldown = 0f;
 	private float Button2CooldownRate = 3f;
 
-	Vector3 LauncherPosition;
+	Vector3 FrontLauncherPosition;
+	Vector3 BackLauncherPosition;
+	
+	bool IsOrigianlPosition;
 
 	public Color StartColor;
 	public Color EndColor;
 
-	private Renderer LauncherRenderer;
+	private Renderer BackLauncherRenderer;
+	private Renderer FrontLauncherRenderer;
 
 	AudioSource AS;
 	public AudioClip FirstWeaponClip;
 	public AudioClip BeepSound;
+	public AudioClip ChangePositionClip;
 
 	bool CannotAttack = false;
 
@@ -50,29 +49,41 @@ public class WonjuJo_BasicWeapon : MonoBehaviour
 		button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
         button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
 
-		LauncherRenderer = Barrel.GetComponent<Renderer>();
+		BackLauncherRenderer = BackBarrel.GetComponent<Renderer>();
+		FrontLauncherRenderer = FrontBarrel.GetComponent<Renderer>();
 
-		if (!LauncherRenderer)
-			Debug.Log("There is no LauncherRenderer");
+		IsOrigianlPosition = true;
 
-		LauncherRenderer.material.color = StartColor;
+		BackLauncherRenderer.material.color = StartColor;
+		FrontLauncherRenderer.material.color = StartColor;
 
 		AS = GetComponent<AudioSource>();
+
 	}
 
     void Update()
 	{
-		LauncherPosition = ProjectileLauncher.transform.position;
+		FrontLauncherPosition = FrontProjectileLauncher.transform.position;
+		BackLauncherPosition = BackProjectileLauncher.transform.position;
 
-		if ((Input.GetButtonDown(button1)) && (weaponOut == false) && Time.time > Button1Cooldown)
+		if (Input.GetButtonDown(button1))
 		{
-			Button1Cooldown = Time.time + Button1CooldownRate;
-
-			AS.PlayOneShot(FirstWeaponClip);
-
-			weaponThrust.transform.Translate(0, 0, thrustAmount);
-			weaponOut = true;
-			StartCoroutine(WithdrawWeapon());
+			if(IsOrigianlPosition) // changed
+            {
+				AS.PlayOneShot(ChangePositionClip);
+				StartCoroutine(WaitForSeconds(1.3f));
+				FrontBarrel.SetActive(true);
+				BackBarrel.SetActive(false);
+				IsOrigianlPosition = false;
+            }
+            else //original
+            {
+				AS.PlayOneShot(ChangePositionClip);
+				StartCoroutine(WaitForSeconds(1.3f));
+				FrontBarrel.SetActive(false);
+				BackBarrel.SetActive(true);
+				IsOrigianlPosition = true;
+			}
 			
 		}
 
@@ -80,21 +91,45 @@ public class WonjuJo_BasicWeapon : MonoBehaviour
 		{
 			StartTime = Time.time;
 
-			LauncherRenderer.material.color = StartColor;
+			if (IsOrigianlPosition) // back
+			{
+				BackLauncherRenderer.material.color = StartColor;
 
-			Button2Cooldown = Time.time + Button2CooldownRate;
+				Button2Cooldown = Time.time + Button2CooldownRate;
 
-			Instantiate(Projectile, LauncherPosition, transform.rotation);
-			
-			ProjectileCheck = true;
+				Instantiate(Projectile, BackLauncherPosition, BackProjectileLauncher.transform.rotation);
 
-			CannotAttack = true;
+				ProjectileCheck = true;
+
+				CannotAttack = true;
+			}
+			else //front
+			{
+				FrontLauncherRenderer.material.color = StartColor;
+
+				Button2Cooldown = Time.time + Button2CooldownRate;
+
+				Instantiate(Projectile, FrontLauncherPosition, FrontProjectileLauncher.transform.rotation);
+
+				ProjectileCheck = true;
+
+				CannotAttack = true;
+			}
+
         }
 
         if (CannotAttack)
 		{
-			float t = (Time.time - StartTime) / Button2CooldownRate;
-			LauncherRenderer.material.color = Color.Lerp(EndColor, StartColor, t);
+			if (IsOrigianlPosition) // changed
+			{
+				float t = (Time.time - StartTime) / Button2CooldownRate;
+				BackLauncherRenderer.material.color = Color.Lerp(EndColor, StartColor, t);
+			}
+			else //original
+			{
+				float t = (Time.time - StartTime) / Button2CooldownRate;
+				FrontLauncherRenderer.material.color = Color.Lerp(EndColor, StartColor, t);
+			}
 		}
 
 		if(ProjectileCheck)
@@ -114,16 +149,8 @@ public class WonjuJo_BasicWeapon : MonoBehaviour
 
 	}
 
-	IEnumerator WithdrawWeapon()
-	{
-		yield return new WaitForSeconds(0.6f);
-		weaponThrust.transform.Translate(0, 0, -thrustAmount);
-		weaponOut = false;
-	}
-
 	IEnumerator WaitForSeconds(float time)
     {
 		yield return new WaitForSeconds(time);
-
 	}
 }
