38a6972f94f3f69b1b2ea597d33a45ae4a2aaa38
Coby Colson <coby.colson@digipen.edu>
2022-04-15 09:18:20 -0700

    Coby Colson - SFX

--- prototyping3_2022/Assets/Scripts/Scripts_Students/A20_CobyColson/A20_Snowball.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A20_CobyColson/A20_Snowball.cs
@@ -32,7 +32,10 @@ public class A20_Snowball : MonoBehaviour
     private float action1CooldownTimer = 0.0f;
     private float snowmanTopMiddleHeightDelta;
     private bool bodyResetRequired = false;
-
+    private AudioSource audioSrc;
+    public AudioClip snowballThrowSfx;
+    public AudioClip snowballRollSfx;
+    public AudioClip snowmanGrowSfx;
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +46,8 @@ public class A20_Snowball : MonoBehaviour
 
         snowmanTopMiddleHeightDelta = snowmanTopRef.transform.position.y -
         snowmanMiddleRef.transform.position.y;
+
+        audioSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -62,38 +67,68 @@ public class A20_Snowball : MonoBehaviour
             snowmanBottomRef.SetActive(true);
 
             bodyResetRequired = false;
+
+            audioSrc.clip = snowmanGrowSfx;
+            audioSrc.pitch = 1;
+            audioSrc.volume = 1;
+            audioSrc.Play();
         }
 
         if (Input.GetButtonDown(activekey1) && snowmanBottomRef.activeSelf && transform.up.y >= 1)
         {
-            snowmanBottomRef.SetActive(false);
-
-            snowmanMiddleRef.transform.position = new Vector3(snowmanMiddleRef.transform.position.x,
-                snowmanMiddleRef.transform.position.y - 1.25f,
-                snowmanMiddleRef.transform.position.z);
-
-            snowmanTopRef.transform.position = new Vector3(snowmanTopRef.transform.position.x,
-                snowmanMiddleRef.transform.position.y + snowmanTopMiddleHeightDelta,
-                snowmanTopRef.transform.position.z);
-
-            GameObject turret;
-            turret = Instantiate(snowballBigPrefab) as GameObject;
-            A20_SnowTurretBehavior turretBehavior = turret.GetComponent<A20_SnowTurretBehavior>();
-            turretBehavior.Initialize(snowballPrefab, transform.position + transform.forward * 3.0f, transform.forward, this);
+            RollSnowball();
         }
         if (Input.GetButtonDown(activeKey2) && action1CooldownTimer <= 0)
         {
-            A20_SnowballBehavior snowball = Instantiate(snowballPrefab).GetComponent<A20_SnowballBehavior>();
-            Vector3 spawnPoint = (snowballSpawnLeft ? snowballSpawnPointLeft.position : snowballSpawnPointRight.position);
-            Vector3 spawnDirection = transform.forward + 0.1f * (snowballSpawnLeft ? transform.right : -transform.right);
-            snowball.Initialize(spawnPoint, spawnDirection);
-            snowballSpawnLeft = !snowballSpawnLeft;
-            action1CooldownTimer = action1Cooldown;
+            ThrowSnowball();
         }
     }
     
     public void ResetBody()
     {
         bodyResetRequired = true;
+    }
+
+    public void PlaySnowballSfx(float volume)
+    {
+        audioSrc.clip = snowballThrowSfx;
+        audioSrc.volume = volume;
+        audioSrc.pitch = Random.Range(0.65f, 1.25f);
+        audioSrc.Play();
+    }
+
+    private void ThrowSnowball()
+    {
+        A20_SnowballBehavior snowball = Instantiate(snowballPrefab).GetComponent<A20_SnowballBehavior>();
+            Vector3 spawnPoint = (snowballSpawnLeft ? snowballSpawnPointLeft.position : snowballSpawnPointRight.position);
+            Vector3 spawnDirection = transform.forward + 0.1f * (snowballSpawnLeft ? transform.right : -transform.right);
+            snowball.Initialize(spawnPoint, spawnDirection);
+            snowballSpawnLeft = !snowballSpawnLeft;
+            action1CooldownTimer = action1Cooldown;
+
+            PlaySnowballSfx(0.75f);
+    }
+
+    private void RollSnowball()
+    {
+        snowmanBottomRef.SetActive(false);
+
+        snowmanMiddleRef.transform.position = new Vector3(snowmanMiddleRef.transform.position.x,
+            snowmanMiddleRef.transform.position.y - 1.25f,
+            snowmanMiddleRef.transform.position.z);
+
+        snowmanTopRef.transform.position = new Vector3(snowmanTopRef.transform.position.x,
+            snowmanMiddleRef.transform.position.y + snowmanTopMiddleHeightDelta,
+            snowmanTopRef.transform.position.z);
+
+        GameObject turret;
+        turret = Instantiate(snowballBigPrefab) as GameObject;
+        A20_SnowTurretBehavior turretBehavior = turret.GetComponent<A20_SnowTurretBehavior>();
+        turretBehavior.Initialize(snowballPrefab, transform.position + transform.forward * 3.0f, transform.forward, this);
+
+        audioSrc.clip = snowballRollSfx;
+        audioSrc.pitch = 1;
+        audioSrc.volume = 1;
+        audioSrc.Play();
     }
 }
