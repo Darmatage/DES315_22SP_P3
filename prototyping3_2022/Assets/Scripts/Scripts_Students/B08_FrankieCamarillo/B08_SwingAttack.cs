99f78e829c4143032e17db2356364f06e9d1c85e
Frankie Camarillo <frankiecama@live.com>
2022-04-21 18:13:19 -0700

    finished up my bot

--- prototyping3_2022/Assets/Scripts/Scripts_Students/B08_FrankieCamarillo/B08_SwingAttack.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B08_FrankieCamarillo/B08_SwingAttack.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class B08_SwingAttack : MonoBehaviour
 {
-    public float WindUp = 2.0f;
-    private float CurrentWindUp = 0.0f;
-    public float SwingVelocity = 25.0f;
-    public float StartingRotation = 0.0f;
+	public float damage = 5f;
+	private GameHandler gameHandler;
 
-    public Transform Weapon;
-    public Rigidbody WeaponBody;
+	public GameObject particlesPrefab;
+	public Vector3 SpawnParticlesHere;
 
-    private GameObject Parent;
+	public bool isPlayer1Weapon = false;
+	public bool isPlayer2Weapon = false;
+	public bool isMonsterWeapon = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (!Weapon)
-            Debug.Log("Weapon was not properly set");
+	public void Start()
+	{
+		if (GameObject.FindWithTag("GameHandler") != null)
+		{
+			gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+		}
 
+		if (gameObject.transform.root.tag == "Player1") { isPlayer1Weapon = true; }
+		if (gameObject.transform.root.tag == "Player2") { isPlayer2Weapon = true; }
+	}
 
-    }
+	void OnCollisionEnter(Collision other)
+	{
+		//if (other.relativeVelocity.magnitude > 5) {
+		//get impact location
+		SpawnParticlesHere = other.contacts[0].point;
+		//make particles
+		GameObject damageParticles = Instantiate(particlesPrefab, SpawnParticlesHere, other.transform.rotation);
+		StartCoroutine(destroyParticles(damageParticles));
+		//}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	IEnumerator destroyParticles(GameObject particles)
+	{
+		yield return new WaitForSeconds(0.5f);
+		Destroy(particles);
+	}
 }
--- prototyping3_2022/Assets/Scripts/Scripts_Students/B08_FrankieCamarillo/B08_Weapons.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B08_FrankieCamarillo/B08_Weapons.cs
@@ -10,15 +10,55 @@ public class B08_Weapons : MonoBehaviour
 	public string button3;
 	public string button4; // currently boost in player move script
 
+	public GameObject Pivot;
+
+	private float StartingAngle;
+
+	private bool SwingReady;
+	private bool SwingInProgress;
+	private float currentY = 0.0f;
+	private float buildUp = 0.1f;
+
+
 	void Start()
 	{
 		button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
 		button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
 		button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
 		button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
+
+		SwingReady = true;
+		SwingInProgress = false;
+		
+		
+
 	}
     void Update()
     {
-        
-    }
+		if ((Input.GetButtonDown(button1)) && (SwingReady == true))
+		{
+			SwingInProgress = true;
+			SwingReady = false;
+			StartCoroutine(WithdrawWeapon());
+		}
+
+		if ((SwingInProgress == true))
+        {
+			
+			Pivot.transform.Rotate(0, Time.deltaTime*2160, 0);
+			currentY += Time.deltaTime * 2160;
+			if (currentY >= 360)
+            {
+				currentY = 0.0f;
+				SwingInProgress=false;
+            }
+
+		}
+	}
+
+	IEnumerator WithdrawWeapon()
+	{
+		yield return new WaitForSeconds(3.0f);
+		SwingReady = true;
+	}
 }
