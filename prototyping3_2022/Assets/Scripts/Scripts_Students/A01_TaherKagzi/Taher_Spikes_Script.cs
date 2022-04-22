9ce33a3f1b7aa301d57a74460ed7b96ee9b5afba
Tskcool <tskcool123@gmail.com>
2022-04-21 23:55:17 -0700

    Adjusted buggy spike hitbox

--- prototyping3_2022/Assets/Scripts/Scripts_Students/A01_TaherKagzi/Taher_Spikes_Script.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A01_TaherKagzi/Taher_Spikes_Script.cs
@@ -7,13 +7,18 @@ public class Taher_Spikes_Script : MonoBehaviour
     //NOTE: This script goes on the main playerBot Game Object, and the weapon goes in the public GO slot
 
 	public GameObject[] spikeHolders;
-	public GameObject[] jumpers;
-	private float thrustAmount = -0.6f;
+  public GameObject[] jumpers;
+	public GameObject[] spikeMats;
+	private float thrustAmount = -0.71f;
+	private float thrustAmountJumpers = -0.6f;
 	private float timer = 0.0f;
-	public float Spike_cd = 5.0f;
+	public float Spike_cd = 4.0f;
 
 	private bool weaponOut = false;
 	private bool jumpOut = false;
+
+	public Material readyMat;
+	public Material notReadyMat;
 
 	//grab axis from parent object
 	public string button1;
@@ -28,20 +33,44 @@ public class Taher_Spikes_Script : MonoBehaviour
 		button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
 		button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
 		jumpButton = gameObject.transform.parent.GetComponent<playerParent>().jumpInput;
+		timer = Spike_cd;
+
+		foreach (var spike in spikeHolders)
+		{
+			spike.SetActive(false);
+		}
 	}
 
     void Update(){
 		//if (Input.GetKeyDown(KeyCode.T)){
 		timer += Time.deltaTime;
 
+		if (timer > Spike_cd)
+		{
+			foreach (var holder in spikeMats)
+			{
+				holder.GetComponent<MeshRenderer>().material = readyMat;
+			}
+		}
+
+		else
+		{
+			foreach (var holder in spikeMats)
+			{
+				holder.GetComponent<MeshRenderer>().material = notReadyMat;
+			}
+		}
+
 		if ((Input.GetButtonDown(button1))&&(weaponOut==false) && timer > Spike_cd){
 
 			foreach (var spike in spikeHolders)
 			{
 				spike.transform.Translate(0, thrustAmount, 0);
+				spike.SetActive(true);
 			}
-			
+
 			weaponOut = true;
+			timer = 0;
 			
 			StartCoroutine(WithdrawWeapon());
 		}
@@ -51,7 +80,7 @@ public class Taher_Spikes_Script : MonoBehaviour
 
 			foreach (var jump in jumpers)
 			{
-				jump.transform.Translate(-thrustAmount, 0, 0);
+				jump.transform.Translate(-thrustAmountJumpers, 0, 0);
 			}
 
 			jumpOut = true;
@@ -61,11 +90,12 @@ public class Taher_Spikes_Script : MonoBehaviour
 	}
 
 	IEnumerator WithdrawWeapon(){
-		yield return new WaitForSeconds(0.6f);
+		yield return new WaitForSeconds(0.3f);
 
 		foreach (var spike in spikeHolders)
 		{
 			spike.transform.Translate(0, -thrustAmount, 0);
+			spike.SetActive(false);
 		}
 
 		weaponOut = false;
@@ -77,7 +107,7 @@ public class Taher_Spikes_Script : MonoBehaviour
 
 		foreach (var jump in jumpers)
 		{
-			jump.transform.Translate(thrustAmount, 0, 0);
+			jump.transform.Translate(thrustAmountJumpers, 0, 0);
 		}
 
 		jumpOut = false;
