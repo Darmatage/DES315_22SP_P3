25b54d7fe88057c8aeafaf964d42f5b8e3cf9319
BarBenzvi <65105160+BarBenzvi@users.noreply.github.com>
2022-04-22 16:19:30 -0700

    Charge sound and particle effects

--- prototyping3_2022/Assets/Scripts/Scripts_Students/B05_BarBenzvi/B05_BotFrontWeapon.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/B05_BarBenzvi/B05_BotFrontWeapon.cs
@@ -7,12 +7,15 @@ public class B05_BotFrontWeapon : MonoBehaviour
     public GameObject WeaponParent = null;
     public GameObject RightWing = null;
     public GameObject LeftWing = null;
+    public ParticleSystem ChargeSystem = null;
+    public AudioClip ChargeSound = null;
     public float DashSpeed = 10.0f;
     public float Cooldown = 3.5f;
     public float WeaponActiveTime = 0.5f;
 
     string button1;
     Rigidbody rb = null;
+    AudioSource source = null;
     float timer = 0.0f;
     float weaponActiveTimer = 0.0f;
 
@@ -20,7 +23,13 @@ public class B05_BotFrontWeapon : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
         button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
+        if(ChargeSystem != null)
+        {
+            ParticleSystem.EmissionModule module = ChargeSystem.emission;
+            module.rateOverDistanceMultiplier = 0;
+        }
     }
 
     // Update is called once per frame
@@ -38,11 +47,25 @@ public class B05_BotFrontWeapon : MonoBehaviour
             rb.AddForce(transform.forward * DashSpeed, ForceMode.Impulse);
             timer = Cooldown;
             weaponActiveTimer = WeaponActiveTime;
+            if (ChargeSystem != null)
+            {
+                ParticleSystem.EmissionModule module = ChargeSystem.emission;
+                module.rateOverDistanceMultiplier = 10;
+            }
+            if(source != null && ChargeSound != null)
+            {
+                source.PlayOneShot(ChargeSound);
+            }
         }
 
         if(WeaponParent != null && weaponActiveTimer <= 0.0f)
         {
             WeaponParent.SetActive(false);
+            if (ChargeSystem != null)
+            {
+                ParticleSystem.EmissionModule module = ChargeSystem.emission;
+                module.rateOverDistanceMultiplier = 0;
+            }
         }
     }
 }
