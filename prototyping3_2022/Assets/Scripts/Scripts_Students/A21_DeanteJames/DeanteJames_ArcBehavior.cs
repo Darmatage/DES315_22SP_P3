45abf08602e6995aba846fc4fbbc85d367842749
Deante James <d.dj@digipen.edu>
2022-04-23 02:07:35 -0700

    Final changes and bug fixes to my bot

--- prototyping3_2022/Assets/Scripts/Scripts_Students/A21_DeanteJames/DeanteJames_ArcBehavior.cs
+++ prototyping3_2022/Assets/Scripts/Scripts_Students/A21_DeanteJames/DeanteJames_ArcBehavior.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class DeanteJames_ArcBehavior : MonoBehaviour
 {
     public bool player1 = false;
+    public GameObject musicManager;
+    public AudioClip zap;
 
     // Start is called before the first frame update
     void Start()
     {
+        musicManager = GameObject.Find("MusicManager");
         if (player1)
         {
             gameObject.GetComponent<HazardDamage>().isPlayer1Weapon = true;
@@ -25,12 +28,17 @@ public class DeanteJames_ArcBehavior : MonoBehaviour
         
     }
 
-    // Make so anything but the ground collisions is ignored for the
+    // Make it so anything but the ground collisions is ignored for the
     // arc and the coil
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name.Contains("Ground"))
         { return; }
+        if (musicManager && !collision.gameObject.name.Contains("Coil") && !collision.gameObject.name.Contains("BotA21"))
+        {
+            musicManager.GetComponent<AudioSource>().PlayOneShot(zap);
+        }
+
         Physics.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider>());
     }
 
